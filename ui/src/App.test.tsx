c65f16660c9b4af9a63d1dfc73cd67dba82b79ce
Guillermo Gutierrez <guillegr123@gmail.com>
2020-12-30 16:13:24 -0600

    Delete React UI app

--- ui/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("should render", () => {
-  const { getByText } = render(<App />);
-  const header = getByText("Welcome to Bootzooka!");
-  expect(header).toBeInTheDocument();
-});
--- ui/src/pages/SecretMain/SecretMain.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import SecretMain from "./SecretMain";
-
-test("renders text content", () => {
-  const { getByText } = render(<SecretMain />);
-  const header = getByText(/Shhhh, this is a secret place./i);
-  expect(header).toBeInTheDocument();
-});
